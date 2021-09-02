4029a8a9f996862158e8da76e283709d3682207e
Justin Reese <justaddcoffee@gmail.com>
2021-09-02 12:59:35 -0700

    Add trivial test

---
+++ tests/test_version.py
@@ -0,0 +1,12 @@
+import logging
+import tempfile
+from unittest import TestCase
+
+
+class TestRunTransform(TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_reality(self) -> None:
+        self.assertTrue(True)
