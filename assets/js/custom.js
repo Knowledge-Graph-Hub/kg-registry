75b4d4ca06646afa286c5c5f2deae1dc57cc9721
caufieldjh <j.harry.caufield@gmail.com>
2025-02-19 11:28:14 -0500

    Quality Control -> License on table heading

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -102,7 +102,7 @@ jQuery(document).ready(function() {
                             </th>
                             <th scope="col">
                                 <span class="sort-button" title="Sort by status" data-sort="license" >
-                                    Quality Control <i class="bi-chevron-up" aria-hidden="true"></i>
+                                    License <i class="bi-chevron-up" aria-hidden="true"></i>
                                 </span>
                             </th>
                         </tr>
